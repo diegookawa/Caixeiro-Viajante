9b55c6395fdfa42f0936ddcc7f081b483411db03
diegookawa <diego.arenas1505@gmail.com>
2021-11-25 20:09:26 -0300

    Funcoes para grafos, HEAP e pontos

--- caixeiroViajante.c
+++ caixeiroViajante.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
-#include <stdbool.h>
 #include <math.h>
 
 typedef struct no {
@@ -52,7 +51,6 @@ int pai(int i);
 int existe(VerticeCusto V[], int vertice, int tam);
 int retornarPosicao(VerticeCusto V[], int vertice, int tam);
 void trocar(VerticeCusto V[], int a, int b);
-void imprimir(VerticeCusto V[], int size);
 void atualizarHeapMinimo(VerticeCusto V[], int size, int i);
 void criarHeapMinimo(VerticeCusto V[], int size);
 void diminuirValorChave(VerticeCusto V[], int i, int chave, int size);
@@ -139,90 +137,90 @@ void iniciarProdecessores(Grafo *grafo, int prodecessores[]){
 
 int pai (int i) {
 
-   return (i - 1) / 2;
+    return (i - 1) / 2;
 
 }
 
-void atualizarHeapMinimo(VerticeCusto V[], int size, int i) {
+void atualizarHeapMinimo(VerticeCusto V[], int size, int i){
 
-   int esquerda = (i * 2) + 1;
-   int direita = (i * 2) + 2;
-   int menor;
+    int esquerda = (i * 2) + 1;
+    int direita = (i * 2) + 2;
+    int menor;
 
-   if((esquerda < size) && (V[esquerda].custo < V[i].custo))   
-      menor = esquerda;
+    if((esquerda < size) && (V[esquerda].custo < V[i].custo))   
+       menor = esquerda;
    
-   else
-      menor = i;
+    else
+       menor = i;
 
-   if((esquerda < size) && (V[direita].custo < V[menor].custo))
-      menor = direita;
+    if((esquerda < size) && (V[direita].custo < V[menor].custo))
+       menor = direita;
 
-   if(menor != i){
+    if(menor != i){
 
-      trocar(V, i, menor);
-      atualizarHeapMinimo(V, size, menor);
+       trocar(V, i, menor);
+       atualizarHeapMinimo(V, size, menor);
 
-   }
-
-}
-
-void trocar(VerticeCusto V[], int a, int b) {
-
-   VerticeCusto aux = V[a];
-   V[a] = V[b];
-   V[b] = aux;
+    }
 
 }
 
-void criarHeapMinimo(VerticeCusto V[], int size) {
+void trocar(VerticeCusto V[], int a, int b){
 
-   for(int i = 0; i < size/2; i++)
-      atualizarHeapMinimo(V, size, i);
-
-}
-
-VerticeCusto extrairMinimo(VerticeCusto V[], int *size) {
-
-   VerticeCusto vc;
-   int min;
-
-   if((*size) < 1)
-      printf("Erro: heap underflow");
-
-   vc = V[0];
-   V[0] = V[(*size) - 1];
-   (*size)--;
-   atualizarHeapMinimo(V, *size, 0);
-
-   return vc;
+    VerticeCusto aux = V[a];
+    V[a] = V[b];
+    V[b] = aux;
 
 }
 
-void diminuirValorChave(VerticeCusto V[], int i, int chave, int size) {
+void criarHeapMinimo(VerticeCusto V[], int size){
 
-   if(chave > V[i].custo){
+    for(int i = 0; i < size/2; i++)
+       atualizarHeapMinimo(V, size, i);
 
-      printf("Erro: chave maior que o atual");
-      return;
+}
 
-   }
+VerticeCusto extrairMinimo(VerticeCusto V[], int *size){
 
-   V[i].custo = chave;
-   while(i > 0 && V[pai(i)].custo > V[i].custo){
+    VerticeCusto verticeCusto;
+    int min;
 
-      trocar(V, i, pai(i));
-      i = pai(i);
+    if((*size) < 1)
+       printf("Erro: heap underflow");
+
+    verticeCusto = V[0];
+    V[0] = V[(*size) - 1];
+    (*size)--;
+    atualizarHeapMinimo(V, *size, 0);
+
+    return verticeCusto;
+
+}
+
+void diminuirValorChave(VerticeCusto V[], int i, int chave, int size){
+
+    if(chave > V[i].custo){
+
+       printf("Erro: chave maior que o atual");
+       return;
+
+    }
+
+    V[i].custo = chave;
+    while(i > 0 && V[pai(i)].custo > V[i].custo){
+
+       trocar(V, i, pai(i));
+       i = pai(i);
    
-   }
+    }
 
 }
 
-void inserirHeapMinimo(VerticeCusto V[], int chave, int *size) {
+void inserirHeapMinimo(VerticeCusto V[], int chave, int *size){
 
-   (*size)++;
-   V[(*size) - 1].custo = INT_MAX;
-   diminuirValorChave(V, (*size) - 1, chave, *size);
+    (*size)++;
+    V[(*size) - 1].custo = INT_MAX;
+    diminuirValorChave(V, (*size) - 1, chave, *size);
 
 }
 
